740d4ff69cc8a4f835abc3d3c6e5bf778471b748
Al Tobey <tobert@gmail.com>
2013-02-19 13:42:00 +0000

    WIP: add test, fill in a bit more, use Vfs
    Not much to see here yet. Just checking in at the end of the day
    and for the first time, there is a test and it passes, so that's
    worth a commit.

--- src/lnxns/cgroups_linux.go
+++ src/lnxns/cgroups_linux.go
@@ -8,13 +8,47 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
+	"path"
+	"os"
 )
 
 type Cgroup struct {
+	Name        string
+	root        *Vfs
+	isMountRoot bool // is this a mount root or unified mount?
+	controllers []string
 }
 
-func NewCgroup(name string) (cg *Cgroup) {
-	return nil
+// create a new cgroup, the Vfs provided should already point
+// to the root of cgroup mount(s).  If the cgroup already exists
+// it is _not_ read automatically, you should call .Load() if you
+// want to start with what the kernel has.
+// root, err := FindCgroupRoot()
+// root, err := NewVfs("/sys/fs/cgroup")
+// root, err := NewVfs("/sys/fs/cgroup/memory")
+// cg := NewCgroup(root, "tobert")
+func NewCgroup(vfs *Vfs, name string) (cg *Cgroup, err error) {
+	// see if we're in a controller mount or a root
+	// start by looking for a "tasks" file, if it's there, vfs is either a single
+	// controller mount or monolithic, either way just scan the files to see which
+	// controllers are enabled and call it done. Otherwise, it's a root like /sys/fs/cgroup
+	// and all path construction is a little different.
+
+	// TODO: Write this for real ...
+	taskFile := path.Join(vfs.Path(), "tasks")
+
+	rs, err := os.Stat(taskFile)
+	if err != nil {
+		fmt.Printf("no such file!? %s\n", err)
+		return
+	}
+	rs.Mode()
+	return
+}
+
+// search for controllers in the root Vfs and return them
+func ListAvailableControllers() (list []string, err error) {
+	return
 }
 
 func (cg *Cgroup) AddController() {
@@ -22,7 +56,10 @@ func (cg *Cgroup) AddController() {
 }
 
 func (cg *Cgroup) Controllers() (list []string) {
-	return nil
+	for _, vfs := range cg.controllers {
+		list = append(list, string(vfs))
+	}
+	return
 }
 
 // add a process by pid, automatically getting all threads
@@ -39,19 +76,31 @@ func (cg *Cgroup) Tasks() (list []int) {
 	return
 }
 
+// load settings from an existing cgroup, returns ENOENT if the
+// cgroup doesn't already exist
+func (cg *Cgroup) Load() (err error) {
+	return
+}
+
+// compare in-memory values to what the kernel has, returns
+// a bool and a string diff (format is not guaranteed)
+func (cg *Cgroup) Verify() (match bool, diff string, err error) {
+	return
+}
+
 func (cg *Cgroup) Apply() (err error) {
 	return
 }
 
 // finds where cgroups are mounted and returns the path string
-func FindMountPath() (path string, err error) {
+func FindCgroupRoot() (vfs *Vfs, err error) {
 	mtab, err := ioutil.ReadFile("/proc/mounts")
 	assertNil(err, "Could read /proc/mounts")
 
 	for mount := range strings.Split(string(mtab), "\n") {
 		fmt.Print(mount)
 	}
-	return "", err
+	return
 }
 
 // vim: ts=4 sw=4 noet tw=120 softtabstop=4
---
+++ src/lnxns/cgroups_linux_test.go
@@ -0,0 +1,28 @@
+// Copyright 2013 Albert P. Tobey. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package lnxns_test
+
+import (
+	"../../src/lnxns"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestCgroup(t *testing.T) {
+	tmpPath, _ := ioutil.TempDir(os.TempDir(), "test-lnxns-cgroups")
+	os.Mkdir(tmpPath, 0755)
+	defer os.RemoveAll(tmpPath)
+
+	os.Mkdir(path.Join(tmpPath, "memory"), 0755)
+	os.Mkdir(path.Join(tmpPath, "blkio"), 0755)
+
+	vr, _ := lnxns.NewVfs(tmpPath)
+	vr.SetString("memory/memory.swappiness", "0")
+	vr.SetString("memory/tasks", "123\n456\n789")
+}
+
+// vim: ts=4 sw=4 noet tw=120 softtabstop=4
